ced2ef6c10e8b48bf56fc96e2d96e23dfc964c0e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-05-08 07:20:19 +0100

    Nineteenth Commit: Implement handlers() function

--- js/App.js
+++ js/App.js
@@ -28,4 +28,35 @@ export default class App {
         this.activeNote = note;
         this.view.updateActiveNote(note);
     }
+
+    _handlers() {
+        return {
+            onNoteSelect: noteId => {
+                const selectedNote = this.notes.find(note => note.id == noteId);
+                this._setActiveNote(selectedNote);
+            },
+            onNoteAdd: () => {
+                const newNote = {
+                    title: "New Note",
+                    body: "Take note..."
+                };
+
+                NotesAPI.saveNote(newNote);
+                this._refreshNotes();
+            },
+            onNoteEdit: (title, body) => {
+                NotesAPI.saveNote({
+                    id: this.activeNote.id,
+                    title,
+                    body
+                });
+
+                this._refreshNotes();
+            },
+            onNoteDelete: noteId => {
+                NotesAPI.deleteNote(noteId);
+                this._refreshNotes();
+            },
+        };
+    }
 }
